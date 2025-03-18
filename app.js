e00f77f2fe71061a159790d90da9be209b8d90f9
Michell-Valenzuela30 <146589327+Michell-Valenzuela30@users.noreply.github.com>
2025-03-18 12:52:59 -0400

    Creando función para añadir amigos
    Creando función para añadir amigos

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+  // Capturar el valor del campo de entrada
+  let nombreAmigo = document.getElementById("amigo").value;
+  
+  // Validar la entrada
+  if (nombreAmigo.trim() === "") { //cuando encuentra vacío muestra un aviso
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+  
+  // Actualizar el array de amigos
+  amigos.push(nombreAmigo);//añade un miembre o integrante
+  
+  actualizarListaAmigos(amigos);
+  
+  // Limpiar el campo de entrada
+  document.getElementById("amigo").value = "";
+}
+
+
+
